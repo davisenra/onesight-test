88217766de2370ad6be52b5bf182cf139b3b7d40
Davi Senra <davi.senra98@gmail.com>
2023-08-26 00:59:28 -0300

    fix: https was not working on local environment

--- spa/vite.config.js
+++ spa/vite.config.js
@@ -2,9 +2,20 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), mkcert()],
+    server: {
+        https: true,
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            protocol: 'wss',
+            host: 'localhost',
+            port: 5173
+        }
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
